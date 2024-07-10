6ae83b884362c8abb4d87e849678cc554ebc6fee
FartherSuzume <115266215+FartherSuzume@users.noreply.github.com>
2024-07-11 00:14:38 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById("footer-site-logo").addEventListener('click',()=>{
+    window.location.href = 'https://www.farthersuzume.jp/';
+});
+
+document.getElementById("site_logo").addEventListener('click',()=>{
+    window.location.href = 'https://www.farthersuzume.jp/';
+});
+
+document.getElementById("end").addEventListener('click',()=>{
+    window.location.href = 'https://www.farthersuzume.jp/';
+});
